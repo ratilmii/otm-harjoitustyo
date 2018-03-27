565b7ad6268ee2eebd21e5b15d8dc41e6d974ed4
ramirami <ratilmii@gmail.com>
2018-03-27 23:48:36 +0300

    ohjelmointitehtävistä se mitä ehdin tehdä

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -10,11 +10,34 @@ public class MaksukorttiTest {
 
     @Before
     public void setUp() {
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(1000);
     }
 
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
+    }
+    
+    @Test
+    public void kortinSaldoAlussaOikein() {
+        assertEquals(1000, kortti.saldo());
+    }
+    
+    @Test 
+    public void lataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(200);
+        assertEquals("saldo: 12.0", kortti.toString());
+    }
+    
+    @Test 
+    public void rahanOttaminenToimii() {
+        kortti.otaRahaa(500);
+        assertEquals("saldo: 5.0", kortti.toString());
+    }
+    
+    @Test
+    public void rahanOttaminenEiToimi() {
+        kortti.otaRahaa(1500);
+        assertEquals("saldo: 10.0", kortti.toString());
     }
 }
