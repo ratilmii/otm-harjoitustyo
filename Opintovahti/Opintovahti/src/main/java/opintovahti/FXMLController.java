3c19fd0dae780570457e3b4ec68d79bdb23ba5a5
ramirami <ratilmii@gmail.com>
2018-04-17 21:41:53 +0300

    Liikuteltava login-screen

--- Opintovahti/Opintovahti/src/main/java/opintovahti/FXMLController.java
+++ Opintovahti/Opintovahti/src/main/java/opintovahti/FXMLController.java
@@ -3,14 +3,21 @@ package opintovahti;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 
 public class FXMLController implements Initializable {
     
+    
     @FXML
     private Label label;
+    
+    @FXML
+    private Pane pane;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
@@ -20,10 +27,12 @@ public class FXMLController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+ 
     }
     
     public void newUserCreated() {
         System.out.println("Uusi käyttäjä luotu.");
     }
+    
+    
 }
--- Opintovahti/Opintovahti/src/main/java/opintovahti/MainApp.java
+++ Opintovahti/Opintovahti/src/main/java/opintovahti/MainApp.java
@@ -2,20 +2,41 @@ package opintovahti;
 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 
 public class MainApp extends Application {
 
+    private double xOffset = 0; 
+    private double yOffset = 0;
+    
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(final Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
         
-        BorderPane bp = new BorderPane();
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                primaryStage.setX(event.getScreenX() - xOffset);
+                primaryStage.setY(event.getScreenY() - yOffset);
+            }
+        });
         
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
@@ -23,6 +44,8 @@ public class MainApp extends Application {
         primaryStage.setTitle("Opintovahti");
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
+        
+            
         primaryStage.show();
     }
 
